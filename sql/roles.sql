ab3428cbd0d5c4553956feb7a8a0b870327a65ab
KoenLuyten <koenluyten@gmail.com>
2023-12-13 13:21:57 +0000

    fix sql script

--- sql/roles.sql
+++ sql/roles.sql
@@ -3,17 +3,17 @@ BEGIN
     CREATE USER [##placeholder_identity##] FROM EXTERNAL PROVIDER;
 END
 
-IF NOT EXISTS (SELECT 1 FROM sys.database_role_members WHERE member_principal_id = USER_ID('##placeholder_identity##') AND role_principal_id = DATABASE_ROLE_ID('db_datareader'))
+IF NOT EXISTS (SELECT 1 FROM sys.database_role_members rm JOIN sys.database_principals r ON rm.role_principal_id = r.principal_id JOIN sys.database_principals m ON rm.member_principal_id = m.principal_id WHERE member_principal_id = USER_ID('##placeholder_identity##') AND r.name = 'db_datareader')
 BEGIN
     ALTER ROLE db_datareader ADD MEMBER [##placeholder_identity##];
 END
 
-IF NOT EXISTS (SELECT 1 FROM sys.database_role_members WHERE member_principal_id = USER_ID('##placeholder_identity##') AND role_principal_id = DATABASE_ROLE_ID('db_datawriter'))
+IF NOT EXISTS (SELECT 1 FROM sys.database_role_members rm JOIN sys.database_principals r ON rm.role_principal_id = r.principal_id JOIN sys.database_principals m ON rm.member_principal_id = m.principal_id WHERE member_principal_id = USER_ID('##placeholder_identity##') AND r.name = 'db_datawriter')
 BEGIN
     ALTER ROLE db_datawriter ADD MEMBER [##placeholder_identity##];
 END
 
-IF NOT EXISTS (SELECT 1 FROM sys.database_role_members WHERE member_principal_id = USER_ID('##placeholder_identity##') AND role_principal_id = DATABASE_ROLE_ID('db_ddladmin'))
+IF NOT EXISTS (SELECT 1 FROM sys.database_role_members rm JOIN sys.database_principals r ON rm.role_principal_id = r.principal_id JOIN sys.database_principals m ON rm.member_principal_id = m.principal_id WHERE member_principal_id = USER_ID('##placeholder_identity##') AND r.name = 'db_ddladmin')
 BEGIN
     ALTER ROLE db_ddladmin ADD MEMBER [##placeholder_identity##];
 END
